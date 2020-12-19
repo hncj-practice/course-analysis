8e6a6aab86a1fe50437abfe4db73b5b89118ddc1
Tanyiqu <1953649096@qq.com>
2020-12-19 16:15:50 +0800

    添加中文摘要

---
+++ 数据库/课程助手数据.sql
@@ -0,0 +1,10 @@
+-- 添加班级
+INSERT INTO `kczs`.`bj` (`bj_bh`, `bj_mc`) VALUES ('0814171', '计算机科学与技术一班');
+INSERT INTO `kczs`.`bj` (`bj_bh`, `bj_mc`) VALUES ('0814172', '计算机科学与技术二班');
+
+-- 添加学生
+
+
+-- 添加课程
+
+
